cddae7f06b8bb7c5863f9b240d7a13d4a2a99b4e
FerOrrego <fer_orrego99@hotmail.com>
2023-12-19 21:27:15 -0300

    Proyecto Finalizado

---
+++ src/App.jsx
@@ -0,0 +1,146 @@
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Filtros from './components/Filtros';
+import ListadoGastos from './components/ListadoGastos';
+import Modal from './components/Modal';
+import { generarId } from './helpers';
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import { object } from 'prop-types';
+
+function App() {
+  const [gastos, setGastos] = useState([
+    ...(JSON.parse(localStorage.getItem("gastos")) ?? []),
+  ])
+
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  );
+  
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+
+  const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+
+  const [gastoEditar, setGastoEditar] = useState({})
+
+  const [filtro, setFiltro] = useState('')
+  const [gastosFiltrados, setGastosFiltrados] = useState([])
+
+  useEffect(() => {
+    if(Object.keys(gastoEditar).length > 0) {
+      setModal(true)
+
+      setTimeout(() => {
+        setAnimarModal(true)
+      }, 150)
+    }
+  }, [gastoEditar])
+
+  //Guardar en localstorage, y ademas de que si hay alho guardado en localstorage automaticamente se va a la pagina de gastos
+  useEffect(() => {
+    localStorage.setItem('presupuesto', presupuesto ?? 0)
+  }, [presupuesto ])
+
+  useEffect(() => {
+    if(filtro) {
+      //Filtrar gastos por categoria
+      const gastosFiltrados = gastos.filter( gastos => gastos.categoria === filtro )
+      setGastosFiltrados(gastosFiltrados)
+    }
+  }, [filtro])
+   
+  useEffect(() => {
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? [])
+  }, [gastos ])
+
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem('presupuesto', presupuesto)) ?? 0
+    if(presupuestoLS > 0){
+      setIsValidPresupuesto(true)
+    }
+  }, [])
+  
+  const handleNuevoGasto = () => {
+    setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 150)
+  }
+
+  //Actualizar o crear nuevo gasto
+  const guardarGasto = gasto => {
+    if(gasto.id) {
+      //Actualizar gasto
+      const gastosActualizado = gastos.map( gastoState => gastoState.id === gasto.id ? gasto : gastoState)
+      setGastos(gastosActualizado)
+      setGastoEditar({})
+    } else {
+      //Nuevo gasto
+      gasto.id = generarId();
+      gasto.fecha = Date.now();
+      setGastos([...gastos, gasto])    
+    }
+
+    setAnimarModal(false)
+    setTimeout(() => {
+        setModal(false)
+      }, 500)
+  }
+
+  //Eliminar Gastos
+  const eliminarGasto = id => {
+    const gastosActualizados = gastos.filter( gasto => gasto.id !== id);
+    setGastos(gastosActualizados)
+  }
+
+  return (
+    <div className={modal ? 'fijar' : ''}>
+      <Header
+        gastos={gastos} 
+        setGastos={setGastos}
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isValidPresupuesto && (
+        <>
+          <main>
+            <Filtros
+              filtro={filtro}
+              setFiltro={setFiltro}
+            />
+            <ListadoGastos 
+              gastos={gastos}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+              filtro={filtro}
+              gastosFiltrados={gastosFiltrados}
+            />
+          </main>
+          <div className="nuevo-gasto">
+            <img 
+              src={IconoNuevoGasto} 
+              alt="icono nuevo gasto" 
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>  
+      )}
+
+      {modal && <Modal 
+                  setModal={setModal}
+                  animarModal={animarModal}
+                  setAnimarModal={setAnimarModal}
+                  guardarGasto={guardarGasto}
+                  gastoEditar={gastoEditar}
+                  setGastoEditar={setGastoEditar}
+                />}
+
+    </div>
+  )
+}
+
+export default App
